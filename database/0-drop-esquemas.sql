fa6cd827e9b15a81670364fec649a93867e96bfa
José P. Ramos <jpramos2910@gmail.com>
2025-10-25 19:34:58 -0300

    Se cambian scripts de creación de tablas
    Se reemplazan SEQUENCE por INDENTITY, ya que es más moderno y directo

--- database/0-drop-esquemas.sql
+++ database/0-drop-esquemas.sql
@@ -1,10 +1,3 @@
-DROP SEQUENCE seq_evento_id;
-DROP SEQUENCE seq_servidor_acceso_id;
-DROP SEQUENCE seq_usuario_proyecto_id;
-DROP SEQUENCE seq_servidor_id;
-DROP SEQUENCE seq_proyecto_id;
-DROP SEQUENCE seq_usuario_id;
-
 DROP TABLE evento;
 DROP TABLE servidor_acceso;
 DROP TABLE usuario_proyecto;
